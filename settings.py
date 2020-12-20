9458788255fe6af7e3dfe90cfdd1ff58ee02f4c9
Zavyalov Denis <zavyalov.den@yandex.ru>
2020-12-20 16:47:07 +0300

    lesson4

--- settings.py
+++ settings.py
@@ -1,9 +1,13 @@
 from middleware import demo
-from views import index, contacts
+from views import index, contacts, create_course, create_category, category_list, course_list
 
 urls = {
     '/': index,
-    '/contacts/': contacts
+    '/contacts/': contacts,
+    '/create_course/': create_course,
+    '/create_category/': create_category,
+    '/category_list/': category_list,
+    '/course_list/': course_list,
 }
 
 middleware = [
